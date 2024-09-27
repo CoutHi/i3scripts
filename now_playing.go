5a1141304a413aa400ce94d5cd4dc51f1b5dc7ae
Couthi <alicftc@proton.me>
2024-09-27 16:19:32 +0200

    updated the logic, no more exit status 1

--- now_playing.go
+++ now_playing.go
@@ -7,40 +7,42 @@ import (
 )
 
 func main(){
+    
+    var collection Media
+
     artist := exec.Command("sh", "-c", "playerctl metadata artist")
     output, err := artist.Output()
     if err != nil {
-        fmt.Println("Error:", err)
-        return
+        collection.artist = ""
+    }else {
+        collection.artist = strings.TrimSpace(string(output))
     }
 
-    var collection Media;
-
-    collection.artist = strings.TrimSpace(string(output))
 
     album := exec.Command("sh", "-c", "playerctl metadata album")
     output, err = album.Output()
     if err != nil {
-        fmt.Println("Error:", err)
-        return
+        collection.album = ""
+    }else {
+        collection.album = strings.TrimSpace(string(output))
     }
-
-    collection.album = strings.TrimSpace(string(output))
 
     title := exec.Command("sh", "-c", "playerctl metadata title")
     output, err = title.Output()
     if err != nil {
-        fmt.Println("Error:", err)
-        return
+        collection.title = ""
+    }else {
+        collection.title = strings.TrimSpace(string(output))
     }
 
-    collection.title = strings.TrimSpace(string(output))
-
-    if collection.album != "" {
+    if collection.album == "" && collection.title == "" && collection.artist == "" {
+        fmt.Print("[]")
+    }else if collection.album != "" {
         fmt.Print("[",collection.artist,"]"," [",collection.album,"] ","[",collection.title,"]")
     }else {
         fmt.Print("[",collection.artist,"]"," [",collection.title,"]")
     }
+
 }
 
 type Media struct {
